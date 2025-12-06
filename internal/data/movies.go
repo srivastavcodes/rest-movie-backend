69d8e6f5cd19a3719d1d0739d312737441af19bc
Parth Srivastav <parthsrivastav.00@gmail.com>
2025-12-06 21:47:00 +0530

    refactor movie model methods to support context and improve genre handling

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -6,11 +6,10 @@ import (
 	"errors"
 	"fmt"
 	"log/slog"
+	"strings"
 	"time"
 
 	"FernArchive/internal/validator"
-
-	"github.com/lib/pq"
 )
 
 type Movie struct {
@@ -18,63 +17,127 @@ type Movie struct {
 	CreatedAt time.Time `json:"-"`
 	Title     string    `json:"title"`
 	Runtime   Runtime   `json:"runtime,omitempty"`
-	Year      int32     `json:"year,omitempty"`
+	Year      time.Time `json:"year,omitempty"`
 	Genres    []string  `json:"genres,omitempty"`
-	Version   int32     `json:"version"`
 }
 
 type MovieModel struct {
 	DB *sql.DB
 }
 
-func (mdl *MovieModel) Insert(movie *Movie) error {
-	query := `INSERT INTO movies (title, year, runtime, genres)
-		    VALUES ($1, $2, $3, $4)
-		    RETURNING id, created_at, version`
+func (mdl *MovieModel) Insert(ctx context.Context, movie *Movie) error {
+	if len(movie.Genres) < 1 {
+		return errors.New("genres must not be empty")
+	}
+	errFn := func(err error) error {
+		return fmt.Errorf("InsertMovie failed: %v", err)
+	}
+	tx, err := mdl.DB.BeginTx(ctx, nil)
+	if err != nil {
+		return errFn(err)
+	}
+	defer func() { _ = tx.Rollback() }()
 
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-	defer cancel()
+	placeholders := strings.Repeat("?,", len(movie.Genres))
+	placeholders = placeholders[:len(placeholders)-1]
+	var (
+		query = fmt.Sprintf(`SELECT id FROM genres WHERE genres.name IN (%s)`, placeholders)
+		args  = make([]any, 0, len(movie.Genres))
+	)
+	for _, genre := range movie.Genres {
+		args = append(args, genre)
+	}
+	rows, err := tx.QueryContext(ctx, query, args...)
+	if err != nil {
+		return errFn(err)
+	}
+	defer func(rows *sql.Rows) {
+		if err := rows.Close(); err != nil {
+			slog.Error("Failed to close rows: ", "err", err)
+		}
+	}(rows)
+	var genreIds []int64
 
-	args := []any{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres)}
-	return mdl.DB.QueryRowContext(ctx, query, args...).Scan(&movie.Id, &movie.CreatedAt, &movie.Version)
+	for rows.Next() {
+		var gid int64
+		if err := rows.Scan(&gid); err != nil {
+			return errFn(err)
+		}
+		genreIds = append(genreIds, gid)
+	}
+	if err = rows.Err(); err != nil {
+		return errFn(err)
+	}
+	if len(genreIds) != len(movie.Genres) {
+		return errFn(fmt.Errorf("genres mismatch"))
+	}
+	res, err := tx.ExecContext(ctx, `INSERT INTO movies (title, year, runtime) VALUES (?, ?, ?)`,
+		movie.Title, movie.Year, movie.Runtime,
+	)
+	if err != nil {
+		return errFn(err)
+	}
+	movie.Id, err = res.LastInsertId()
+	if err != nil {
+		return errFn(err)
+	}
+	for _, gid := range genreIds {
+		_, err := tx.ExecContext(ctx, `INSERT INTO movie_genres (movie_id, genre_id) VALUES (?, ?)`,
+			movie.Id, gid,
+		)
+		if err != nil {
+			return errFn(err)
+		}
+	}
+	if err = tx.Commit(); err != nil {
+		return fmt.Errorf("failed to commit transaction: %w", err)
+	}
+	return nil
 }
 
-func (mdl *MovieModel) Get(id int64) (*Movie, error) {
-	if id < 1 {
+func (mdl *MovieModel) Get(ctx context.Context, movieId int64) (*Movie, error) {
+	if movieId < 1 {
 		return nil, ErrRecordNotFound
 	}
-	query := `SELECT id, created_at, title, year, runtime, genres, version FROM movies
-                WHERE id = $1`
-	movie := new(Movie)
+	errFn := func(err error) error {
+		return fmt.Errorf("GetMovie failed: %v", err)
+	}
+	query := `SELECT m.id, m.created_at, m.title, m.year, m.runtime, GROUP_CONCAT(g.name)
+		    FROM movies AS m 
+		    LEFT JOIN movie_genres AS mg ON mg.movie_id = m.id
+                LEFT JOIN genres AS g ON g.id = mg.genre_id WHERE m.id = ? GROUP BY m.id`
 
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-	defer cancel()
+	row := mdl.DB.QueryRowContext(ctx, query, movieId)
 
-	err := mdl.DB.QueryRowContext(ctx, query, id).Scan(&movie.Id, &movie.CreatedAt,
-		&movie.Title, &movie.Year, &movie.Runtime, pq.Array(&movie.Genres), &movie.Version)
+	var m Movie
+	var genres string
+
+	err := row.Scan(&m.Id, &m.CreatedAt, &m.Title, &m.Year, &m.Runtime, &genres)
 	if err != nil {
 		switch {
 		case errors.Is(err, sql.ErrNoRows):
 			return nil, ErrRecordNotFound
 		default:
-			return nil, err
+			return nil, errFn(err)
 		}
 	}
-	return movie, nil
+	if genres != "" {
+		m.Genres = strings.Split(genres, ",")
+	}
+	return &m, nil
 }
 
-func (mdl *MovieModel) GetAll(title string, genres []string, fltr Filters) ([]*Movie, Metadata, error) {
-	query := fmt.Sprintf(`
-                    SELECT COUNT(*) OVER(), id, created_at, title, year, runtime, genres, version 
-                    FROM movies
-                    WHERE (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) OR $1 = '')
-   			  AND (genres @> $2 OR $2 = '{}') 
-                    ORDER BY %s %s, id ASC LIMIT $3 OFFSET $4`, fltr.sortParam(), fltr.sortOrder())
+func (mdl *MovieModel) GetAllByTitle(ctx context.Context, title string, ftr Filters) ([]*Movie, Metadata, error) {
+	query := fmt.Sprintf(
+		`SELECT m.id, m.created_at, m.title, m.year, m.runtime, GROUP_CONCAT(DISTINCT g.name) 
+			  FROM movies AS m 
+			  LEFT JOIN movie_genres AS mg ON mg.movie_id = m.id
+            	  LEFT JOIN genres AS g ON g.id = mg.genre_id 
+			  WHERE LOWER(m.title) LIKE LOWER(CONCAT('%', ?, '%'))
+			  GROUP BY m.id ORDER BY %s %s, id ASC LIMIT ? OFFSET ?`, ftr.sortParam(), ftr.sortOrder(),
+	)
+	args := []any{title, ftr.limit(), ftr.offset()}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-	defer cancel()
-
-	args := []any{title, pq.Array(genres), fltr.limit(), fltr.offset()}
 	rows, err := mdl.DB.QueryContext(ctx, query, args...)
 	if err != nil {
 		return nil, Metadata{}, err
@@ -84,63 +147,106 @@ func (mdl *MovieModel) GetAll(title string, genres []string, fltr Filters) ([]*M
 			slog.Error("Failed to close rows: ", "err", err)
 		}
 	}(rows)
-
-	var totalRecords = 0
-	var movies []*Movie
-
+	var (
+		movies   []*Movie
+		genres   string
+		metadata Metadata
+	)
 	for rows.Next() {
-		var movie Movie
-		err := rows.Scan(&totalRecords, &movie.Id,
-			&movie.CreatedAt,
-			&movie.Title,
-			&movie.Year,
-			&movie.Runtime,
-			pq.Array(&movie.Genres),
-			&movie.Version,
-		)
+		var m Movie
+		err := rows.Scan(&m.Id, &m.CreatedAt, &m.Title, &m.Year, &m.Runtime, &genres)
 		if err != nil {
-			return nil, Metadata{}, err
+			return nil, metadata, err
 		}
-		movies = append(movies, &movie)
+		if genres != "" {
+			m.Genres = strings.Split(genres, ",")
+		}
+		movies = append(movies, &m)
 	}
 	if err = rows.Err(); err != nil {
-		return nil, Metadata{}, err
+		return nil, metadata, err
 	}
-	metadata := calculateMetadata(totalRecords, fltr.Page, fltr.PageSize)
+	metadata = calculateMetadata(len(movies), ftr.Page, ftr.PageSize)
 	return movies, metadata, nil
 }
 
-func (mdl *MovieModel) Update(movie *Movie) error {
-	query := `UPDATE movies SET title=$1, year=$2, runtime=$3, genres=$4, version=version+1
-                WHERE id = $5 AND version=$6 RETURNING version`
-
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-	defer cancel()
-
-	args := []any{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres), movie.Id,
-		movie.Version}
-	err := mdl.DB.QueryRowContext(ctx, query, args...).Scan(&movie.Version)
+func (mdl *MovieModel) Update(ctx context.Context, movie *Movie) error {
+	errFn := func(err error) error {
+		return fmt.Errorf("UpdateMovie failed: %v", err)
+	}
+	tx, err := mdl.DB.BeginTx(ctx, nil)
 	if err != nil {
-		switch {
-		case errors.Is(err, sql.ErrNoRows):
-			return ErrEditConflict
-		default:
-			return err
+		return errFn(err)
+	}
+	defer func() { _ = tx.Rollback() }()
+	var (
+		query = `UPDATE movies SET title=?, runtime=?, year=? WHERE id = ?`
+		args  = []any{movie.Title, movie.Runtime, movie.Year, movie.Id}
+	)
+	res, err := tx.ExecContext(ctx, query, args...)
+	if err != nil {
+		return errFn(err)
+	}
+	row, err := res.RowsAffected()
+	if err != nil {
+		return errFn(err)
+	}
+	if row != 1 {
+		return ErrEditConflict
+	}
+	genreMap := make(map[string]int64)
+
+	rows, err := tx.QueryContext(ctx, `SELECT id, name FROM genres`)
+	if err != nil {
+		return errFn(err)
+	}
+	defer func() {
+		if err := rows.Close(); err != nil {
+			slog.Error("Failed to close rows: ", "err", err)
 		}
+	}()
+	for rows.Next() {
+		var (
+			genre_id int64
+			name     string
+		)
+		if err := rows.Scan(&genre_id, &name); err != nil {
+			return errFn(err)
+		}
+		genreMap[name] = genre_id
+	}
+	if err = rows.Err(); err != nil {
+		return errFn(err)
+	}
+	query = `DELETE FROM movie_genres WHERE movie_id = ?`
+
+	_, err = tx.ExecContext(ctx, query, movie.Id)
+	if err != nil {
+		return errFn(err)
+	}
+	query = `INSERT INTO movie_genres (movie_id, genre_id) VALUES (?, ?)`
+
+	for _, genre := range movie.Genres {
+		genre_id, ok := genreMap[genre]
+		if !ok {
+			return errFn(fmt.Errorf("unknown genre %s", genre))
+		}
+		_, err = tx.ExecContext(ctx, query, movie.Id, genre_id)
+		if err != nil {
+			return errFn(err)
+		}
+	}
+	if err = tx.Commit(); err != nil {
+		return errFn(fmt.Errorf("failed to commit transaction: %w", err))
 	}
 	return nil
 }
 
-func (mdl *MovieModel) Delete(id int64) error {
+func (mdl *MovieModel) Delete(ctx context.Context, id int64) error {
 	if id < 1 {
 		return ErrRecordNotFound
 	}
-	query := `DELETE FROM movies WHERE id = $1`
-
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-	defer cancel()
-
-	result, err := mdl.DB.ExecContext(ctx, query, id)
+	result, err := mdl.DB.ExecContext(ctx, `DELETE FROM movies WHERE id = ?`, id)
 	if err != nil {
 		return err
 	}
@@ -148,7 +254,7 @@ func (mdl *MovieModel) Delete(id int64) error {
 	if err != nil {
 		return err
 	}
-	if rows == 0 {
+	if rows != 1 {
 		return ErrRecordNotFound
 	}
 	return nil
