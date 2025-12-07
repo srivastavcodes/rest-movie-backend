10dd87044693192631a0a65c24f17d3694f80554
Parth Srivastav <parthsrivastav.00@gmail.com>
2025-12-07 11:58:33 +0530

    refactor: movie id made bigint

--- migrations/000001_create_movies_table.up.sql
+++ migrations/000001_create_movies_table.up.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS movies
 (
-    id         INT AUTO_INCREMENT PRIMARY KEY,
+    id         BIGINT AUTO_INCREMENT PRIMARY KEY,
     created_at TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
     title      VARCHAR(50) NOT NULL,
-    year       DATETIME    NOT NULL,
+    year       INT         NOT NULL,
     runtime    INT         NOT NULL
 );
 
@@ -15,7 +15,7 @@ CREATE TABLE IF NOT EXISTS genres
 
 CREATE TABLE IF NOT EXISTS movie_genres
 (
-    movie_id INT,
+    movie_id BIGINT,
     genre_id INT,
     PRIMARY KEY (movie_id, genre_id),
     FOREIGN KEY (movie_id) REFERENCES movies (id) ON DELETE CASCADE,
